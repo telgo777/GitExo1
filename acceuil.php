bf61120682482b266421b4a32cab026630c04a4e
etudiant <etudiant@lmdsio.fr>
2025-09-17 15:11:15 +0200

    v1

--- acceuil.php
+++ acceuil.php
@@ -3,6 +3,8 @@ $prenom=$_POST['prenom'];
 
 $nom=$_POST['nom'];
 echo "bonjour".$prenom." ".$nom;
-
+$host = $_SERVER['HTTP_HOST'];
+$uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+header("Location: http://$host$uri/...");
 
 ?>
